c5b1267f6bdb24b1cc19d6b49a9643b1a419f5ff
Austin Pigg <austin_pigg@yahoo.com>
2020-02-29 18:45:59 -0500

    updated JS

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,16 @@ let appId = "1b65b95a63673258b0b08917d8c9ef5c";
 let units = "imperial";
 let searchMethod;
 
+function getSearchMethod(searchTerm) {
+    if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm) {
+        searchMethod = "zip";
+    } else {
+        searchMethod = "q";
+    }
+}
+
 function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
         return result.json();
     }).then(result => {
@@ -23,5 +32,48 @@ function searchWeather(searchTerm) {
 }
 
 function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = 'url("Assets/Photos/clear.jpg")';
+            break;
 
+        case 'Clouds':
+            document.body.style.backgroundImage = 'url("Assets/Photos/cloudy.jpg")';
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+        case 'Mist':
+            document.body.style.backgroundImage = 'url("Assets/Photos/rain.jpg")';
+            break;
+
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = 'url("Assets/Photos/storm.jpg")';
+            break;
+
+        case 'Snow':
+            document.body.style.backgroundImage = 'url("Assets/Photos/snow.jpg")';
+            break;
+    
+        default:
+            document.body.style.backgroundImage = 'url("Assets/Photos/clear.jpg")';
+            break;
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+    let weatherIcon = document.getElementById('documentIconImg');
+    let uVIndex = document.getElementById('UV-Index');
+
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
 }
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+    let searchTerm = document.getElementById("searchInput").value;
+    if(searchTerm) {
+        searchWeather(searchTerm);
+    }
+})
