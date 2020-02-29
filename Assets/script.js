b565e0f98832fe5055ad23d0698cd3bec69d5b64
Austin Pigg <austin_pigg@yahoo.com>
2020-02-29 17:35:56 -0500

    updated JS and CSS

--- Assets/script.js
+++ Assets/script.js
@@ -7,3 +7,21 @@ setInterval(() => {
 
     time.textContent = humanReadable;    
 }, 1000)
+
+
+
+let appId = "1b65b95a63673258b0b08917d8c9ef5c";
+let units = "imperial";
+let searchMethod;
+
+function searchWeather(searchTerm) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+
+}
